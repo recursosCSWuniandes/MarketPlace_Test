21d699c9b70ea63734eea087abc2e24acffa179b
Jonatan <Jonatan@157.253.204.45>
2015-07-01 11:17:20 -0500

    Se realiza ajustes para la galeria

--- MarketPlace.web/src/main/webapp/src/modules/product/product.ctrl.js
+++ MarketPlace.web/src/main/webapp/src/modules/product/product.ctrl.js
@@ -3,11 +3,34 @@
 
     mod.controller('productCtrl', ['$scope', 'productService', 'productModel', function ($scope, svc, model) {
             svc.extendController(this, $scope, model, 'product', 'Product');
-            this.fetchRecords();
-            $scope.addToCar = function(){
-              alert('Added to Cart');     
+            this.fetchRecords(); // Consulta todos los records de la entidad Producto
+            this.readOnly = true; // Habilita el uso de solo lectura (No permite crear nuevas entidades en el toolbar)
+            var self = this;
+            $scope.addToCar = function () {
+                alert('Added to Cart');
+            };
+            
+            this.addToCartList = function(){
+                alert('Added to Cart');       
             };
             
             
+            /*
+             * Metodo que agrega una acción al toobar.
+             * 
+             */
+            this.globalActions.push({
+                name: 'AddtoCart',
+                displayName: 'Add to Cart',
+                icon: 'shopping-cart',
+                fn: function () {
+                    self.addToCartList();
+                },
+                show: function () {
+                    return true;
+                }
+            });
+
+
         }]);
 })(window.angular);
